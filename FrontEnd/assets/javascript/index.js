3fdffa3f8afd8ab71c772fcb799108850e3ca68e
James Rchrdsn <laurent.james63@gmail.com>
2024-01-24 14:57:48 +0100

    Filtres fonctionnelle

--- FrontEnd/assets/javascript/index.js
+++ FrontEnd/assets/javascript/index.js
@@ -13,33 +13,65 @@ getWork();
 async function showWorks() {
   const showImg = await getWork();
   showImg.forEach((element) => {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-    img.src = element.imageUrl;
-    figcaption.textContent = element.title;
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
+    imgArchi(element);
   });
 }
 showWorks();
 
-// Affichage des Boutons Catégories
+// Creation des images dans le dom
+function imgArchi(element) {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  img.src = element.imageUrl;
+  figcaption.textContent = element.title;
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  gallery.appendChild(figure);
+}
+
+// // Affichage des Boutons Catégories
 
 async function getCategories() {
   const apiCategories = await fetch("http://localhost:5678/api/categories");
   return await apiCategories.json();
 }
 getCategories();
-
 async function btnCategories() {
   const categories = await getCategories();
-  categories.forEach((iCategories) => {
+  const allBtn = document.createElement("button");
+  allBtn.textContent = "Tous";
+  allBtn.id = "0";
+  filters.appendChild(allBtn);
+  categories.forEach((categorie) => {
     const btn = document.createElement("button");
-    btn.textContent = iCategories.name;
-    btn.id = iCategories.id;
+    btn.textContent = categorie.name;
+    btn.id = categorie.id;
     filters.appendChild(btn);
   });
 }
 btnCategories();
+
+// Fonction filtres des catégories
+
+async function filtersCat() {
+  const imgFilter = await getWork();
+  const filtersBtn = document.querySelectorAll(".filters button");
+  filtersBtn.forEach((everyBtn) => {
+    everyBtn.addEventListener("click", (e) => {
+      idBtn = e.target.id;
+      gallery.innerHTML = "";
+      if (idBtn !== "0") {
+        const cleanCat = imgFilter.filter((element) => {
+          return element.categoryId == idBtn;
+        });
+        cleanCat.forEach((element) => {
+          imgArchi(element);
+        });
+      } else {
+        showWorks();
+      }
+    });
+  });
+}
+filtersCat();
