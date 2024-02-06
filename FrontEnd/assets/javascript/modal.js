315b490722383e92d1124d990d4997932f15617f
James Rchrdsn <laurent.james63@gmail.com>
2024-02-06 17:24:28 +0100

    almost finish

--- FrontEnd/assets/javascript/modal.js
+++ FrontEnd/assets/javascript/modal.js
@@ -2,6 +2,11 @@ const modalContainer = document.querySelector(".modalContainer");
 const actualGallery = document.createElement("div");
 const xMark = document.createElement("i");
 
+// connexion à l'api
+const token = sessionStorage.getItem("userOnline")
+  ? JSON.parse(sessionStorage.getItem("userOnline")).token
+  : null;
+
 // Fonction qui créer la modal et tout les champs
 // dont elle a besoin et qui gère la fermeture
 function primaryModal() {
@@ -77,9 +82,6 @@ function deleteImg(trash) {
     return;
   }
   //   recuperation de mon état connecté
-  const token = sessionStorage.getItem("userOnline")
-    ? JSON.parse(sessionStorage.getItem("userOnline")).token
-    : null;
 
   if (!token) {
     // si pas de token annulation
@@ -120,6 +122,7 @@ async function displayModal() {
   try {
     const imgModal = await getWork();
     actualGallery.innerHTML = "";
+    gallery.innerHTML = "";
     imgModal.forEach((element) => {
       // creation de chaques images presente dans l'api
       const figure = document.createElement("figure");
@@ -135,6 +138,11 @@ async function displayModal() {
       figure.appendChild(trash);
       figure.appendChild(img);
       actualGallery.appendChild(figure);
+
+      // Ajoute chaque image à la galerie principale
+      const mainPageImage = document.createElement("img");
+      mainPageImage.src = element.imageUrl;
+      gallery.appendChild(mainPageImage);
     });
   } catch (error) {
     console.error("Error fetching works:", error);
@@ -143,71 +151,98 @@ async function displayModal() {
 displayModal();
 
 // Changement de modal ajout de photo
-
+const validate = document.createElement("button");
+const form = document.createElement("form");
+const fileInput = document.createElement("input");
+const titleInput = document.createElement("input");
+const previous = document.createElement("i");
+const secondaryTitle = document.createElement("h3");
+const modulePhoto = document.createElement("div");
+const newPhoto = document.createElement("i");
+const btnPhoto = document.createElement("button");
+const textPhoto = document.createElement("p");
+const titleLabel = document.createElement("label");
+const categorieLabel = document.createElement("label");
+const categorieSelect = document.createElement("select");
+const underBar = document.createElement("div");
+const overlay = document.createElement("div");
 // Fonction qui créer la seconde modal
 function secondaryModal() {
   fetchCategories();
-  const previous = document.createElement("i");
   previous.classList.add("fas", "fa-arrow-left");
   modalContainer.appendChild(previous);
   xMark.classList.add("fas", "fa-times");
   modalContainer.appendChild(xMark);
-  const secondaryTitle = document.createElement("h3");
   secondaryTitle.classList.add("modalTitle");
   secondaryTitle.innerText = "Ajout photo";
   modalContainer.appendChild(secondaryTitle);
-  const modulePhoto = document.createElement("div");
   modulePhoto.classList.add("modulePhoto");
   modalContainer.appendChild(modulePhoto);
-  const newPhoto = document.createElement("i");
   newPhoto.classList.add("far", "fa-image");
   modulePhoto.appendChild(newPhoto);
-  const btnPhoto = document.createElement("button");
   btnPhoto.classList.add("btnPhoto");
   btnPhoto.innerText = "+ Ajouter photo";
   modulePhoto.appendChild(btnPhoto);
-  const textPhoto = document.createElement("p");
   textPhoto.classList.add("textPhoto");
   textPhoto.innerText = "jpg, png : 4mo max";
   modulePhoto.appendChild(textPhoto);
-  const form = document.createElement("form");
   modalContainer.appendChild(form);
-  const titleLabel = document.createElement("label");
   titleLabel.textContent = "Titre";
   form.appendChild(titleLabel);
-  const titleInput = document.createElement("input");
   titleInput.type = "text";
   titleInput.id = "title";
   titleInput.name = "title";
   titleInput.required = true;
   form.appendChild(titleInput);
-  const categorieLabel = document.createElement("label");
   categorieLabel.textContent = "Catégorie";
   form.appendChild(categorieLabel);
-  const categorieSelect = document.createElement("select");
   categorieSelect.id = "categorie";
   categorieSelect.name = "categorie";
   categorieSelect.required = true;
   form.appendChild(categorieSelect);
-  const underBar = document.createElement("div");
   underBar.classList.add("underBar");
   modalContainer.appendChild(underBar);
-  const validate = document.createElement("button");
   validate.classList.add("validateBtn");
   validate.innerText = "Valider";
   modalContainer.appendChild(validate);
   // opacité derriere la modal
-  const overlay = document.createElement("div");
   overlay.classList.add("overlay", "active");
   document.body.appendChild(overlay);
-  // retour sur la premiere modal
-  previous.addEventListener("click", (event) => {
-    closeModal(); // Fermer la modal principale
-    primaryModal(); // Afficher la modal d'ajout de photo
-    event.stopPropagation();
-  });
+  // Ajout de photo
+  fileInput.type = "file";
+  fileInput.id = "imageFile";
+  fileInput.name = "imageFile";
+  // N'accepte que les fichiers jpg, jpeg, png
+  fileInput.accept = ".jpg, .jpeg, .png";
+  // Masquer le champ de fichier
+  fileInput.style.display = "none";
+  modulePhoto.appendChild(fileInput);
 }
 
+// retour sur la premiere modal
+previous.addEventListener("click", (event) => {
+  closeModal();
+  primaryModal();
+  event.stopPropagation();
+});
+// Ouvrir le sélecteur de fichiers lorsque le bouton est cliqué
+btnPhoto.addEventListener("click", () => {
+  fileInput.click();
+});
+
+// Gérer le changement du champ de fichier
+fileInput.addEventListener("change", (event) => {
+  const selectedFile = fileInput.files[0];
+  if (selectedFile) {
+    // Affiche l'image sélectionnée dans la modal
+    const imgPreview = document.createElement("img");
+    imgPreview.src = URL.createObjectURL(selectedFile);
+    modulePhoto.appendChild(imgPreview);
+
+    // Cache le bouton ajout lors de la previsualisation de l'image
+    btnPhoto.classList.add("hidden");
+  }
+});
 // Recupération des catégories
 async function fetchCategories() {
   try {
@@ -226,3 +261,83 @@ async function fetchCategories() {
     console.error("Erreur lors de la récupération des catégories", error);
   }
 }
+
+// Fonction envoie du formulaire
+async function sendData() {
+  // Récupération des valeurs des champs du formulaire
+  const title = document.getElementById("title").value.trim();
+  const category = document.getElementById("categorie").value.trim();
+  const selectedFile = document.getElementById("imageFile").files[0];
+
+  // Vérifie si les champs sont remplis et qu'un fichier est sélectionné
+  if (title !== "" && category !== "" && selectedFile) {
+    try {
+      // Création d'un FormData pour envoyer les données du formulaire
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("category", category);
+      formData.append("image", selectedFile);
+
+      // Envoi des données du formulaire au serveur
+      const response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        // Réinitialiser les champs du formulaire
+        document.getElementById("title").value = "";
+        document.getElementById("imageFile").value = "";
+        // Supprimer l'aperçu de l'image
+        const imgPreview = document.querySelector(".modulePhoto img");
+        if (imgPreview) {
+          imgPreview.remove();
+        }
+
+        // Actualiser l'affichage de la galerie après l'ajout de la photo
+        displayModal();
+      } else {
+        throw new Error("Erreur lors de l'envoi des données");
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'envoi des données :", error);
+    }
+  } else {
+    // Affiche un message d'erreur si tous les champs ne sont pas remplis
+    console.error("Formulaire incomplet");
+  }
+}
+// Envoie le formulaire à l'api
+// soit au click du bouton valider
+validate.addEventListener("click", function (event) {
+  event.preventDefault();
+  sendData();
+});
+// soit quand on appuie sur entrée
+form.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    sendData();
+  }
+});
+
+// Fonction pour vérifier si le formulaire est rempli
+function checkFormCompletion() {
+  // Vérifier si les champs requis sont remplis
+  const isTitleFilled = titleInput.value.trim() !== "";
+  const isFileSelected = fileInput.files.length > 0;
+
+  // Met à jour la classe du bouton de validation
+  if (isTitleFilled && isFileSelected) {
+    validate.classList.add("completedValidate");
+  } else {
+    validate.classList.remove("completedValidate");
+  }
+}
+
+// Ajoute des écouteurs d'événements pour les champs du formulaire
+titleInput.addEventListener("input", checkFormCompletion);
+fileInput.addEventListener("change", checkFormCompletion);
